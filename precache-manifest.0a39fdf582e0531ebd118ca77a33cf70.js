d1e623666dc0c0c9b14991a3f0d9a9558b6e0397
Silentium237 <58401268+Silentium237@users.noreply.github.com>
2020-09-28 00:46:56 +0300

    Updates

---
+++ precache-manifest.0a39fdf582e0531ebd118ca77a33cf70.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e815669cb0d7ff9075bf09db0e36765f",
+    "url": "/vova/index.html"
+  },
+  {
+    "revision": "b0303044945baedf3439",
+    "url": "/vova/static/css/main.28f2d0cf.chunk.css"
+  },
+  {
+    "revision": "139b7c1da932ec7d799e",
+    "url": "/vova/static/js/2.57067ad2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vova/static/js/2.57067ad2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0303044945baedf3439",
+    "url": "/vova/static/js/main.665b4de4.chunk.js"
+  },
+  {
+    "revision": "920bac732dab17125b4f",
+    "url": "/vova/static/js/runtime-main.17ffc7e0.js"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/vova/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/vova/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/vova/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "c64ec5c8d3ea8c3743ca51c1fa59e50e",
+    "url": "/vova/static/media/img-5.c64ec5c8.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/vova/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/vova/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "ebf6a1510a49f429567c192feb0d25ea",
+    "url": "/vova/static/media/img-8.ebf6a151.jpg"
+  }
+]);
